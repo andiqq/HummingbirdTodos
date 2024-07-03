7433b849f0a580a88b602162b31f4d958ccd136d
Andreas Plagens <andreas@plagens.eu>
2024-07-03 17:45:33 +0200

    Testing the application

--- Sources/Todos.swift
+++ Sources/Todos.swift
@@ -3,7 +3,7 @@ import Hummingbird
 import Logging
 
 @main
-struct Todos: AsyncParsableCommand {
+struct Todos: AsyncParsableCommand, AppArguments {
     
     @Option(name: .shortAndLong)
     var hostname: String = "127.0.0.1"
@@ -11,27 +11,43 @@ struct Todos: AsyncParsableCommand {
     @Option(name: .shortAndLong)
     var port: Int = 8080
     
+    @Flag
+    var inMemoryTesting: Bool = false
+    
     func run() async throws {
         
-        var logger = Logger(label: "Todos")
-        logger.logLevel = .debug
-        
-        let router = Router()
-        
-        router.middlewares.add(LogRequestsMiddleware(.info))
-        
-        router.get("/") { request, context in
-            "Hello\n"
-        }
-        
-        TodoController(repository: TodoMemoryRepository()).addRoutes(to: router.group("todos"))
-        
-        let app = Application(
-            router: router,
-            configuration: .init(address: .hostname(self.hostname, port: self.port)),
-            logger: logger
-        )
+        let app = try await buildApplication(self)
         
         try await app.runService()
     }
+}
+
+protocol AppArguments {
+    var hostname: String { get }
+    var port: Int { get }
+    var inMemoryTesting: Bool { get }
+}
+
+func buildApplication(_ args: some AppArguments) async throws -> some ApplicationProtocol {
+    
+    var logger = Logger(label: "Todos")
+    logger.logLevel = .debug
+    
+    let router = Router()
+    
+    router.middlewares.add(LogRequestsMiddleware(.info))
+    
+    router.get("/") { request, context in
+        "Hello\n"
+    }
+    
+    TodoController(repository: TodoMemoryRepository()).addRoutes(to: router.group("todos"))
+    
+    let app = Application(
+        router: router,
+        configuration: .init(address: .hostname(args.hostname, port: args.port)),
+        logger: logger
+    )
+    
+    return app
 }
